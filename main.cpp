426f985ab2f83ff40e0b9d093273a75bc5765522
Sneasel1 <143228744+Sneasel1@users.noreply.github.com>
2024-05-31 13:33:37 +1000

    Updated

--- main.cpp
+++ main.cpp
@@ -4,7 +4,12 @@ void sayHello() {
     std::cout << "Hello, world!\n";
 }
 
+void sayGoodbye() {
+    std::cout << "Cyas!\n";
+}
+
 int main() {
     sayHello();
+    sayGoodbye();
     return 0;
 }
